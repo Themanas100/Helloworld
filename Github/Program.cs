64552621e455da98deb5bce7358496c1b777c20a
Themanas100 <lolking5@hotmail.com>
2017-01-19 14:35:14 +0100

    Klart

---
+++ Github/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gittest gitTest = new Gittest();
+            gitTest.Run();
+        }
+    }
+}
